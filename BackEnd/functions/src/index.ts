29d27772cfe8cc9ddadcb29b5986d0056d241c13
Gabriel de Oliveira Mansur <gb.mansur@gmail.com>
2025-05-28 16:49:29 -0300

    Coloquei o GetLoginStatus no git

--- BackEnd/functions/src/index.ts
+++ BackEnd/functions/src/index.ts
@@ -1,3 +1,4 @@
+import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as QRCode from "qrcode";
 import {onCall, HttpsError} from "firebase-functions/v2/https";
@@ -6,22 +7,24 @@ import {v4 as uuidv4} from "uuid";
 
 admin.initializeApp();
 
+
 export const performAuth = onCall(async (request) => {
   try {
     // 1. Validação de dados
     if (!request.data.APIkey) {
-      throw new HttpsError(
+      throw new functions.https.HttpsError(
         "failed-precondition",
         "Parâmetro APIkey obrigatório"
       );
     }
 
     if (request.data.APIkey !== "API") {
-      throw new HttpsError(
+      throw new functions.https.HttpsError(
         "permission-denied",
         "API key inválida"
       );
     }
+
     // 2. Geração de dados
     const loginToken = uuidv4();
     const dataAtual = admin.firestore.Timestamp.now();
@@ -30,7 +33,7 @@ export const performAuth = onCall(async (request) => {
     const qrcode = await QRCode.toDataURL(loginToken, {type: "image/png"});
 
     // 4. Salvamento no Firestore
-    await admin.firestore().collection("loginTokens").doc(loginToken).set({
+    await admin.firestore().collection("logins").doc(loginToken).set({
       API: request.data.APIkey,
       DataEhorario: dataAtual,
       logintoken: loginToken,
@@ -46,65 +49,67 @@ export const performAuth = onCall(async (request) => {
   } catch (error) {
     // 6. Tratamento de erros
     console.error("Erro na função performAuth:", error);
-    throw new HttpsError(
+    throw new functions.https.HttpsError(
       "internal",
-      ` Falha ao processar requisição ${error}`,
+      `Falha ao processar requisição ${error}`,
     );
   }
 });
 
 
-
-
-export const getLoginStatus = onCall (async (request) =>{
-  if(!request.data.loginToken){
+export const getLoginStatus = onCall(async (request) =>{
+  if (!request.data.loginToken) {
     throw new HttpsError(
-        "failed-precondition",
-        "Parâmetro loginToken Obrigatorio"
-      );
+      "failed-precondition",
+      "Parâmetro loginToken Obrigatorio"
+    );
   }
-  const loginToken = request.data.loginToken
+  const loginToken = request.data.loginToken;
   const tokenDoc =
-  await admin.firestore().collection('loginTokens').doc(loginToken).get();
+  await admin.firestore().collection("logins").doc(loginToken).get();
 
-  if(!tokenDoc.exists){
-     throw new HttpsError(
-        "permission-denied",
-        "Login Token invalida"
-      );
+  if (!tokenDoc.exists) {
+    throw new HttpsError(
+      "permission-denied",
+      "Login Token invalida"
+    );
   }
   const data = tokenDoc.data();
 
-  if(!data){
-     throw new HttpsError(
-        "failed-precondition",
-        "Login token com falta de data"
-      );
+  if (!data) {
+    throw new HttpsError(
+      "failed-precondition",
+      "Login token com falta de data"
+    );
   }
 
   const now = admin.firestore.Timestamp.now();
 
-  const secs = now.seconds - data.DataEhorario.seconds
-  if(secs>60 || data.tentativas<=0){
+  const secs = now.seconds - data.DataEhorario.seconds;
+  if (secs>60 || data.tentativas<=0) {
     await tokenDoc.ref.delete();
-     throw new HttpsError(
-        "aborted",
-        "Acabou o tempo ou o numero de tentativas"
-      );
+    throw new HttpsError(
+      "aborted",
+      "Acabou o tempo ou o numero de tentativas"
+    );
   }
   await tokenDoc.ref.update({
-    tentativas:admin.firestore.FieldValue.increment(-1)
-  })
+    tentativas: admin.firestore.FieldValue.increment(-1),
+  });
 
-  if(data.UserId){
-    const user = 
+  if (data.UserId) {
+    await tokenDoc.ref.update({
+      status: "Completed",
+    });
+    const user =
     await admin.auth().getUser(data.UserId);
     return {
-      uid:user.uid,
-      email:user.email
-    }
+      uid: user.uid,
+      email: user.email,
+    };
   }
 
-  return `tentativas: ${data.tentativas -1}`
+  return `tentativas: ${data.tentativas -1}`;
+});
 
-}); 
+
